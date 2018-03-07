12a5cb867b4386ff32fe3125a4082a8ac21a25fe
ohs60213 <ohs60213@172.23.32.49>
2018-03-07 20:35:15 +0900

    売り上げ情報自動追加プログラム

--- src/vendingmachine/aaa.java
+++ src/vendingmachine/aaa.java
@@ -1,4 +1,4 @@
-package Dao;
+package vendingmachine;
 
 import java.io.IOException;
 import java.sql.ResultSet;
@@ -9,17 +9,19 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import Dao.Dao;
+
 /**
- * Servlet implementation class SampleDB
+ * Servlet implementation class aaa
  */
-@WebServlet("/SampleDB")
-public class SampleDB extends HttpServlet {
+@WebServlet("/aaa")
+public class aaa extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public SampleDB() {
+    public aaa() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,39 +32,22 @@ public class SampleDB extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 
-		int vendingId = 3;
-		int MEMBER_ID = 164;
-		int drink = 0;
-		int EARNINGS＿ID = 0;
-		String sql = "";
-
 		Dao dao = null;
 		ResultSet rs = null;
-
-
+		int productID = 0;
 		try{
 
-			for(int i=0;i<100;i++){
-			drink = (int) (Math.floor(Math.random() * (1 - 12 + 1)) + 12) ;
-			int productID = 0;
-			dao = new Dao();
-			rs = dao.execute("SELECT product_id FROM stock WHERE vending_id ="+vendingId+" ORDER BY RAND() LIMIT 1;");
-			while(rs.next()){
-				productID = rs.getInt("product_id");
+			for(int i=0;i<50;i++){
+
+				dao = new Dao();
+				rs = dao.execute("SELECT product_id FROM stock WHERE vending_id=1 ORDER BY RAND() LIMIT 1;");
+				while(rs.next()){
+					productID = rs.getInt("product_id");
+				}
+
+				System.out.println(productID);
 			}
-			dao = new Dao();
-			rs = dao.execute("SELECT COUNT(*) FROM earnings;");
-			while(rs.next()){
-				EARNINGS＿ID = rs.getInt("COUNT(*)");
-			}
-			EARNINGS＿ID += 1;
-			System.out.println(EARNINGS＿ID);
-			sql = ("INSERT INTO earnings(id, date, member_id, product_id, vending_id, stock_count)" +
-					"VALUES('"+ EARNINGS＿ID +"','2018/03/07 19:16:06','"+MEMBER_ID +"','"+ productID +"','"+vendingId+"','"+drink+"');");
-			dao = new Dao();
-			dao.executeUpdate(sql);
-			}
-			System.out.println("成功");
+
 		}catch(Exception e){
 			System.out.println("エラー");
 		}finally{
